50f121573c072840d484cc0079007b3328fef9f8
Tamara Alani <Tammy@dhcp-10-142-174-40.uawifi.arizona.edu>
2015-09-28 16:30:46 -0700

    changed lcd header

--- lab1p2_205.X/lcd.h
+++ lab1p2_205.X/lcd.h
@@ -1,23 +1,15 @@
-/* 
+/*
  * File:   lcd.h
- * Author: Tammy
+ * Authors:
  *
- * Created on September 28, 2015, 4:14 PM
+ * Created on December 31, 2014, 1:39 PM
  */
 
-#ifndef LCD_H
-#define	LCD_H
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-
-
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif	/* LCD_H */
-
+void writeLCD(unsigned char word, unsigned int commandType, unsigned int delay);
+void writeFourBits(unsigned char word, unsigned int commandType, unsigned int delayAfter, unsigned int lower);
+void initLCD(void);
+void clearLCD(void);
+void moveCursorLCD(unsigned char x, unsigned char y);
+void printCharLCD(char c);
+void printStringLCD(const char* s);
+void testLCD();
