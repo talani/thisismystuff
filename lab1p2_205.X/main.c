1e1f303bda368f5e117fd32e4092bca83f25c2c3
Tamara Alani <Tammy@Tamaras-MacBook-Air.local>
2015-09-28 20:31:10 -0700

    adding part2

---
+++ lab1p2_205.X/main.c
@@ -0,0 +1,34 @@
+// ******************************************************************************************* //
+//
+// File:         lab1p2.c
+// Date:         12-30-2014
+// Authors:      Garrett Vanhoy
+//
+// Description: 
+// ******************************************************************************************* //
+//comment
+#include <xc.h>
+#include <sys/attribs.h>
+#include "lcd.h"
+#include "timer.h"
+#include "config.h"
+#include "interrupt.h"
+
+
+// ******************************************************************************************* //
+
+int main(void)
+{
+    SYSTEMConfigPerformance(40000000);
+    
+    initTimer2();
+    //initLCD();
+    //testLCD();
+    enableInterrupts();
+    while(1)
+    {
+        testLCD();
+    }
+    
+    return 0;
+}
